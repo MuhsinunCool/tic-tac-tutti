6a01c8f06d93be78398c40c8e0858afc5eab7f73
Muhsinun Chowdhury <muhsinun08@gmail.com>
2018-04-15 22:42:33 -0400

    Revert @myy233's test commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const square = document.getElementsByClassName("square");
 
 for (let i = 0; i < square.length; i++) {
   square[i].addEventListener("click", function(event) {
-    if ((square[i].innerHTML === `N`) | (square[i].innerHTML === `O`)) {
+    if ((square[i].innerHTML === `X`) | (square[i].innerHTML === `O`)) {
       console.log("Box already filled with either N or O");
       return;
     } else {
